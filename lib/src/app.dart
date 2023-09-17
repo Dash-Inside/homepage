a9ff79218197db7029a6c384e1093d11d768edb7
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-09-17 13:09:23 +0300

    feat: add web preloader

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:homepage/src/pages/home_page.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(
-        useMaterial3: true,
+    return Animate(
+      effects: const [
+        FadeEffect(
+          delay: Duration(milliseconds: 400),
+          duration: Duration(milliseconds: 800),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark(
+          useMaterial3: true,
+        ),
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+            builder: (_) => const HomePage(),
+          );
+        },
       ),
-      onGenerateRoute: (settings) {
-        return MaterialPageRoute(
-          builder: (_) => const HomePage(),
-        );
-      },
     );
   }
 }
